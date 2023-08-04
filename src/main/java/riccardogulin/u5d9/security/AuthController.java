5fe6ee9a68c4cfd8ece84deec0e031062509a482
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-08-04 10:34:52 +0200

    Fixes Auth Controller

--- src/main/java/riccardogulin/u5d9/security/AuthController.java
+++ src/main/java/riccardogulin/u5d9/security/AuthController.java
@@ -2,20 +2,20 @@ package riccardogulin.u5d9.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import riccardogulin.u5d9.exceptions.UnauthorizedException;
 import riccardogulin.u5d9.users.User;
 import riccardogulin.u5d9.users.UsersService;
+import riccardogulin.u5d9.users.payloads.LoginSuccessfullPayload;
 import riccardogulin.u5d9.users.payloads.UserLoginPayload;
 import riccardogulin.u5d9.users.payloads.UserRequestPayload;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class AuthController {
 	@Autowired
@@ -33,7 +33,7 @@ public class AuthController {
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody UserLoginPayload body) {
+	public LoginSuccessfullPayload login(@RequestBody UserLoginPayload body) {
 		// 1. Verifichiamo che l'email dell'utente sia presente nel db
 
 		User user = usersService.findByEmail(body.getEmail());
@@ -44,7 +44,7 @@ public class AuthController {
 
 			// 3. Se le credenziali sono OK --> genero un JWT e lo invio come risposta
 			String token = jwtTools.createToken(user);
-			return new ResponseEntity<>(token, HttpStatus.OK);
+			return new LoginSuccessfullPayload(token);
 
 		} else {
 			// 4. Se le credenziali NON sono OK --> 401
